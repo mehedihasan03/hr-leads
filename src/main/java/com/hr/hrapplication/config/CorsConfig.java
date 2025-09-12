e2296d33eb8b22dc2a32137fba96dd21acf7d17e
mehedi <mehedi@cellosco.pe>
2025-09-12 19:52:52 +0600

    add cors config

---
+++ src/main/java/com/hr/hrapplication/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.hr.hrapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // all endpoints
+                        .allowedOrigins("http://localhost:4200") // Angular dev server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
